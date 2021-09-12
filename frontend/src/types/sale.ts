03ae0cb34b8c690ba0ac6e4e2f579e1ebb8d870a
Leandro Andrade <leandrofa1980@gmail.com>
2021-09-12 20:19:51 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,11 @@
+// A formatação do meu VSCode tira o ';' quando salvo.
 export type SaleSum = {
-  sellerName: string,
+  sellerName: string
   sum: number
+}
+
+export type SaleSuccess = {
+  sellerName: string
+  visited: number
+  deals: number
 }
