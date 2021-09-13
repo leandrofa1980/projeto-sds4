65169355856432cf505e7a031419767bb6ef3e58
Leandro Andrade <leandrofa1980@gmail.com>
2021-09-12 21:25:14 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,27 @@
+import { Seller } from './seller'
+
 // A formatação do meu VSCode tira o ';' quando salvo.
+export type Sale = {
+  id: number
+  visited: number
+  deals: number
+  amount: number
+  date: string
+  seller: Seller
+}
+
+export type SalePage = {
+  content?: Sale[]
+  last: boolean
+  totalPages: number
+  totalElements: number
+  size?: number
+  number: number
+  first: boolean
+  numberOfElements?: number
+  empty?: boolean
+}
+
 export type SaleSum = {
   sellerName: string
   sum: number
---
+++ frontend/src/types/seller.ts
@@ -0,0 +1,5 @@
+// A formatação do meu VSCode tira o ';' quando salvo.
+export type Seller = {
+  id: number
+  name: string
+}
